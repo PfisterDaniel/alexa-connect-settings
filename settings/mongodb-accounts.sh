7dab1b8d76e9b71bc1709d8b91496d848247d377
Daniel Pfister <kontakt@daniel-pfister.de>
2020-11-30 15:11:18 +0100

    Update mongodb-accounts.sh

--- settings/mongodb-accounts.sh
+++ settings/mongodb-accounts.sh
@@ -4,31 +4,19 @@ set -eu
 mongo -- users<<EOF
     var admin = db.getSiblingDB('admin');
     admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
-
-    var mqDB = 'users'
-    var user = '<web-app-user>';
-    var passwd = '<web-app-password>';
-    db.createUser({user: user, pwd: passwd, roles: [{role: "readWrite", db: mqDB}]});
+    db.createUser({user: "<web-app-user>", pwd: "<web-app-password>", roles: [{role: "readWrite", db: "users"}]});
 EOF
 
 mongo -- sessions<<EOF
     var admin = db.getSiblingDB('admin');
     admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
-
-    var mqDB = 'sessions'
-    var user = '<web-app-user>';
-    var passwd = '<web-app-password>';
-    db.createUser({user: user, pwd: passwd, roles: [{role: "dbOwner", db: mqDB}]});
+    db.createUser({user: "<web-app-user>", pwd: "<web-app-password>", roles: [{role: "dbOwner", db: "sessions"}]});
 EOF
 
 mongo -- admin<<EOF
     var admin = db.getSiblingDB('admin');
     admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
-
-    var mqDB = 'users'
-    var user = '<mqtt-user>';
-    var passwd = '<mqtt-password>';
-    db.createUser({user: user, pwd: passwd, roles: [{role: "read", db: mqDB}]});
+    db.createUser({user: "<mqtt-user>", pwd: "<mqtt-password>", roles: [{role: "read", db: "users"}]});
 EOF
 
 #mongo mongodb://localhost/users --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
