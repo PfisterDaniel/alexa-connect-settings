0342a9b5bf5a21f74de99cff6676842e9549ccf3
Daniel Pfister <kontakt@daniel-pfister.de>
2020-11-30 14:39:48 +0100

    Update mongodb-accounts.sh

--- settings/mongodb-accounts.sh
+++ settings/mongodb-accounts.sh
@@ -1,29 +1,42 @@
-mongo mongodb://localhost/users --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
-    db.createUser({
-        user: "<web-app-user>",
-        pwd: "<web-app-password>",
-        roles: [{
-            role: "readWrite",
-            db: "users"
-        }]
-    })'
+#!/usr/bin/env bash
+set - eu
 
-mongo mongodb://localhost/sessions --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
-    db.createUser({
-        user: "<web-app-user>",
-        pwd: "<web-app-password>",
-        roles: [{
-           role: "dbOwner",
-            db: "sessions"
-        }]
-    })'
+mongo -- users<<EOF
+    var admin = db.getSiblingDB('admin');
+    admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
+
+    var mqDB = 'users'
+    var user = '<mqtt-user>';
+    var passwd = '<mqtt-password>';
+    db.createUser({user: user, pwd: passwd, roles: [{role: "read", db: mqDB}]});
+    db.createCollection("users");
+EOF
+#mongo mongodb://localhost/users --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
+#    db.createUser({
+#        user: "<web-app-user>",
+#        pwd: "<web-app-password>",
+#        roles: [{
+#            role: "readWrite",
+#            db: "users"
+#        }]
+#   })'
+
+#mongo mongodb://localhost/sessions --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
+#    db.createUser({
+#       user: "<web-app-user>",
+#        pwd: "<web-app-password>",
+#        roles: [{
+#           role: "dbOwner",
+#            db: "sessions"
+#        }]
+#    })'
     
-mongo mongodb://localhost/admin --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
-    db.createUser({
-        user: "<mqtt-user>",
-        pwd: "<mqtt-password>",
-        roles: [{
-            role: "read",
-            db: "users"
-        }]
-    })'
+#mongo mongodb://localhost/admin --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
+#    db.createUser({
+#        user: "<mqtt-user>",
+#        pwd: "<mqtt-password>",
+#        roles: [{
+#            role: "read",
+#            db: "users"
+#        }]
+#    })'
