5c8aaeb4693a032dd2495039996d756c2315f73b
Daniel Pfister <kontakt@daniel-pfister.de>
2020-11-30 15:52:31 +0100

    Update mongodb-accounts.sh

--- settings/mongodb-accounts.sh
+++ settings/mongodb-accounts.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-
 mongo -- users<<EOF
-    var admin = db.getSiblingDB('admin');
-    admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
-    db.createUser({user: "<web-app-user>", pwd: "<web-app-password>", roles: [{role: "readWrite", db: "users"}]});
-EOF
-
-mongo -- sessions<<EOF
-    var admin = db.getSiblingDB('admin');
-    admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
-    db.createUser({user: "<web-app-user>", pwd: "<web-app-password>", roles: [{role: "dbOwner", db: "sessions"}]});
-EOF
-
-mongo -- admin<<EOF
     var admin = db.getSiblingDB('admin');
     admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
     db.createUser({user: "<mqtt-user>", pwd: "<mqtt-password>", roles: [{role: "read", db: "users"}]});
