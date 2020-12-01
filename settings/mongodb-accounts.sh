30080a89ee8282aa8ef81521c074282f3e6ae30f
Daniel Pfister <kontakt@daniel-pfister.de>
2020-12-01 14:35:40 +0100

    Update mongodb-accounts.sh

--- settings/mongodb-accounts.sh
+++ settings/mongodb-accounts.sh
@@ -5,33 +5,3 @@ mongo -- admin<<EOF
     admin.auth('<mongo-admin-user>', '<mongo-admin-password>');
     db.createUser({user: "<mqtt-user>", pwd: "<mqtt-password>", roles: [{role: "read", db: "admin"}]});
 EOF
-
-#mongo mongodb://localhost/users --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
-#    db.createUser({
-#        user: "<web-app-user>",
-#        pwd: "<web-app-password>",
-#        roles: [{
-#            role: "readWrite",
-#            db: "users"
-#        }]
-#   })'
-
-#mongo mongodb://localhost/sessions --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
-#    db.createUser({
-#       user: "<web-app-user>",
-#        pwd: "<web-app-password>",
-#        roles: [{
-#           role: "dbOwner",
-#            db: "sessions"
-#        }]
-#    })'
-    
-#mongo mongodb://localhost/admin --authenticationDatabase admin -u '<mongo-admin-user>' -p '<mongo-admin-password>' --eval '
-#    db.createUser({
-#        user: "<mqtt-user>",
-#        pwd: "<mqtt-password>",
-#        roles: [{
-#            role: "read",
-#            db: "users"
-#        }]
-#    })'
